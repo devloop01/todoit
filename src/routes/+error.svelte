62a5a3c22f48afbf7408cee45e50fa66c2ee6b1a
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-08-08 20:46:11 +0530

    update: root error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="h-screen grid place-content-center text-center bg-gray-100">
-	<h1 class="text-gray-300 font-inter font-extrabold text-9xl">{$page.status}</h1>
-	<h3 class="text-gray-800 font-inter font-medium text-3xl">{$page.error?.message}</h3>
+	<h1 class="text-gray-300 font-inter font-extrabold text-6xl">{$page.status}</h1>
+	<h3 class="text-gray-800 font-inter font-medium text-xl">{$page.error?.message}</h3>
 </div>
