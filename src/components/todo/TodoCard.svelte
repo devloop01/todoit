77446e8acbc0c1345f86a2c3d048914193aa6e05
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-08-09 11:13:47 +0530

    update: todo card layout

--- src/components/todo/TodoCard.svelte
+++ src/components/todo/TodoCard.svelte
@@ -1,35 +1,50 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import type { Todo } from '$lib/types';
 
+	import { enhance } from '$app/forms';
 	import { CheckSquareIcon, SquareIcon, Trash2Icon } from 'lucide-svelte';
 
-	export let todo: Todo;
+	import { cn } from '$lib/utils';
 
-	let formElement: HTMLFormElement;
+	import { Button } from '$components/ui/button';
+	import { Card, CardContent, CardFooter, CardHeader } from '$components/ui/card';
+	import { Label } from '$components/ui/label';
+
+	export let todo: Todo;
 </script>
 
-<div class="p-3 border rounded-xl flex justify-between">
-	<form method="POST" action="?/complete" use:enhance>
-		<label class="cursor-pointer flex items-center gap-3">
-			<button type="submit" aria-label="toggle todo">
+<Card class="flex justify-between items-center">
+	<CardHeader class="p-0 px-4">
+		<form method="POST" action="?/complete" class="flex items-center gap-3" use:enhance>
+			<button aria-label="toggle todo">
 				{#if todo.completed}
 					<CheckSquareIcon size={16} />
 				{:else}
 					<SquareIcon size={16} />
 				{/if}
 			</button>
-			<input type="hidden" name="todo-id" value={todo.id} />
-			<!-- <input type="checkbox" id={todo.id} checked={todo.completed} /> -->
-			<span class:line-through={todo.completed}>{todo.title}</span>
-		</label>
-	</form>
-	{#if todo.completed}
-		<form method="POST" action="?/delete" use:enhance>
-			<input type="hidden" name="todo-id" value={todo.id} />
-			<button type="submit" aria-label="delete todo">
-				<Trash2Icon size={16} />
-			</button>
+			<input class="hidden" type="hidden" name="todo-id" id={todo.id} value={todo.id} />
+			<Label for={todo.id} class="sr-only">
+				{todo.title}
+			</Label>
 		</form>
+	</CardHeader>
+
+	<CardContent class="p-0 flex-1">
+		<a href={`todos/${todo.id}`}>
+			<p class={cn(todo.completed && 'line-through', 'select-none py-4')}>
+				{todo.title}
+			</p>
+		</a>
+	</CardContent>
+	{#if todo.completed}
+		<CardFooter class="p-0 px-2">
+			<form method="POST" action="?/delete" use:enhance>
+				<input type="hidden" name="todo-id" value={todo.id} />
+				<Button variant="ghost" size="icon" aria-label="delete todo">
+					<Trash2Icon size={16} />
+				</Button>
+			</form>
+		</CardFooter>
 	{/if}
-</div>
+</Card>
