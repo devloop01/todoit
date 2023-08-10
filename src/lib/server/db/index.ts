4cf697e4d3cbea3600781910ee42ba45353b4ea1
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-08-10 23:34:21 +0530

    update: db config ^& migration func

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,12 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-import { env } from '$env/dynamic/private';
+import { DATABASE_URL, NODE_ENV } from '$env/static/private';
 
-const connectionString = env.DATABASE_URL;
-const client = postgres(connectionString);
+import * as Schema from './schema';
 
-export const db = drizzle(client);
+const client = postgres(DATABASE_URL, { ssl: NODE_ENV === 'production' ? 'require' : false });
+
+const db = drizzle(client, { schema: Schema });
+
+export { db, Schema };
--- src/lib/server/db/migrate.ts
+++ src/lib/server/db/migrate.ts
@@ -4,22 +4,21 @@ import postgres from 'postgres';
 
 import 'dotenv/config';
 
-export async function migrateDb() {
-	const connectionString = process.env.DATABASE_URL;
-	const client = postgres(connectionString, { max: 1 });
+const connectionString = process.env.DATABASE_URL;
+const client = postgres(connectionString, { max: 1 });
 
-	const db = drizzle(client);
+const db = drizzle(client);
 
-	console.log('Migrating database...');
-	await migrate(db, { migrationsFolder: 'drizzle' });
-	console.log('Database migrated.');
+export async function main() {
+	try {
+		console.log('Migrating database...');
+		await migrate(db, { migrationsFolder: 'drizzle' });
+		console.log('Database migrated.');
+	} catch (err) {
+		console.error(err);
+	}
 
 	process.exit(0);
 }
 
-try {
-	migrateDb();
-} catch (err) {
-	console.error(err);
-	process.exit(1);
-}
+main();
