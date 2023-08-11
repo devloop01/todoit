4343c49a11782baa2927490dcc5042a7439c1146
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-08-11 12:58:28 +0530

    update: add header login btn

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,6 +1,14 @@
+<script>
+	import { buttonVariants } from './ui/button';
+</script>
+
 <header class="bg-white p-4 flex items-center justify-between border-b">
 	<a href="/">
-		<span class="font-inter font-bold text-3xl text-primary">todoit.</span>
+		<span class="font-inter font-bold text-2xl text-primary">todoit.</span>
 	</a>
-	<img src="/avatar.jpg" alt="Avatar" class="h-9 w-9 rounded-full" />
+	{#if true}
+		<a href="/sign-in" class={buttonVariants()}>Sign In</a>
+	{:else}
+		<img src="/avatar.jpg" alt="Avatar" class="h-9 w-9 rounded-full" />
+	{/if}
 </header>
