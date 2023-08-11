1041dc26e8e7361b53ec553d5ce37b5912c9a2a1
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-08-11 12:24:08 +0530

    update: convert id to uuid in db

---
+++ drizzle/0003_steep_hellfire_club.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "todos" ADD COLUMN "id" uuid DEFAULT gen_random_uuid() NOT NULL;
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, boolean, varchar, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, boolean, varchar, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const todos = pgTable('todos', {
-	id: serial('id').primaryKey(),
+	id: uuid('id').defaultRandom().primaryKey(),
 	title: varchar('title', { length: 50 }).notNull(),
 	description: varchar('description', { length: 255 }),
 	completed: boolean('completed').notNull().default(false),
