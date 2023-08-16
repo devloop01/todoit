1c6de5e3e0a5935c646b07d6a182267c4716d364
Sikriti Dakua <destrodev123@gmail.com>
2023-08-17 03:28:21 +0530

    delete: old migration files

--- drizzle/0000_past_bruce_banner.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "todos" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"title" varchar(50) NOT NULL,
-	"decsription" varchar(255),
-	"completed" boolean DEFAULT false NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
