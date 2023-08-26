95202100407806ab21241a628911468ab25f7aa2
Sikriti Dakua <destrodev123@gmail.com>
2023-08-27 00:16:38 +0530

    fix: Schema imports

--- src/lib/server/db/handlers/todos.ts
+++ src/lib/server/db/handlers/todos.ts
@@ -5,37 +5,37 @@ import { desc, eq } from 'drizzle-orm';
 import { Schema, db } from '..';
 
 const createTodo = async (text: string) => {
-	const [newTodo] = await db.insert(Schema.todos).values({ title: text }).returning();
+	const [newTodo] = await db.insert(Schema.todo).values({ title: text }).returning();
 
 	return newTodo;
 };
 
 const getTodo = async (id: string) => {
-	const [todo] = await db.select().from(Schema.todos).where(eq(Schema.todos.id, id));
+	const [todo] = await db.select().from(Schema.todo).where(eq(Schema.todo.id, id));
 
 	return todo;
 };
 
 const getTodos = async () => {
-	return await db.select().from(Schema.todos).orderBy(desc(Schema.todos.createdAt));
+	return await db.select().from(Schema.todo).orderBy(desc(Schema.todo.created_at));
 };
 
 const updateTodo = async (id: string, todo: Partial<NewTodo>) => {
 	const [updatedTodo] = await db
-		.update(Schema.todos)
+		.update(Schema.todo)
 		.set({
 			title: todo.title,
 			description: todo.description,
 			completed: todo.completed
 		})
-		.where(eq(Schema.todos.id, id))
+		.where(eq(Schema.todo.id, id))
 		.returning();
 
 	return updatedTodo;
 };
 
 const deleteTodo = async (id: string) => {
-	await db.delete(Schema.todos).where(eq(Schema.todos.id, id));
+	await db.delete(Schema.todo).where(eq(Schema.todo.id, id));
 };
 
 export { createTodo, getTodo, getTodos, updateTodo, deleteTodo };
