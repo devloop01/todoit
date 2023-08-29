d8f2ecf05570e2359028758f9d3985103e9aeb0c
Sikriti Dakua <destrodev123@gmail.com>
2023-08-29 12:31:33 +0530

    feat: add regex check on auth routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,12 +3,15 @@ import { redirect, type Handle } from '@sveltejs/kit';
 import { auth } from '$lib/server/lucia';
 import { redirectToSignIn } from '$lib/utils/redirect';
 
+const authPaths = ['/login', '/register', '/confirm-email', '/forgot-password', '/reset-password'];
+const authPathsRegex = new RegExp(`^(${authPaths.join('|')})`);
+
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 
 	const session = await event.locals.auth.validate();
 
-	if (event.url.pathname.startsWith('/login') || event.url.pathname.startsWith('/register')) {
+	if (authPathsRegex.test(event.url.pathname)) {
 		if (session?.user) {
 			throw redirect(302, '/app');
 		}
