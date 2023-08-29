687368e1f4656cef47dffefa8676a85a0a5b4aba
Sikriti Dakua <destrodev123@gmail.com>
2023-08-29 23:50:18 +0530

    update: use `event.route` to protect routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,23 +3,18 @@ import { redirect, type Handle } from '@sveltejs/kit';
 import { auth } from '$lib/server/lucia';
 import { redirectToSignIn } from '$lib/utils/redirect';
 
-const authPaths = ['/login', '/register', '/confirm-email', '/forgot-password', '/reset-password'];
-const authPathsRegex = new RegExp(`^(${authPaths.join('|')})`);
-
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 
 	const session = await event.locals.auth.validate();
 
-	if (authPathsRegex.test(event.url.pathname)) {
-		if (session?.user) {
+	if (session) {
+		if (event.route.id?.startsWith('/(auth)')) {
 			throw redirect(302, '/app');
 		}
-	}
-
-	if (event.url.pathname.startsWith('/app')) {
-		if (!session?.user) {
-			throw redirect(302, redirectToSignIn({ url: event.url }));
+	} else {
+		if (event.route.id?.startsWith('/(protected)')) {
+			throw redirect(302, redirectToSignIn({ url: event.url, error: 'auth_required' }));
 		}
 	}
 
