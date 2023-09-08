09974bdd107c49a0867fc7e2c0464af33ba2427e
Sikriti Dakua <destrodev123@gmail.com>
2023-09-08 23:22:35 +0530

    update: `confirm-email` action

--- src/routes/(protected)/confirm-email/+page.server.ts
+++ src/routes/(protected)/confirm-email/+page.server.ts
@@ -1,17 +1,19 @@
-import { redirect, fail } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
+import { redirect } from 'sveltekit-flash-message/server';
 import { generateEmailVerificationToken } from '$lib/server/token';
 import { sendEmailVerificationLink } from '$lib/server/email';
 
 import type { PageServerLoad, Actions } from './$types';
+import { redirectToSignIn } from '$lib/utils/redirect';
 
 export const load: PageServerLoad = async () => {
 	return {};
 };
 
 export const actions: Actions = {
-	default: async ({ locals }) => {
+	default: async ({ locals, url }) => {
 		const session = await locals.auth.validate();
-		if (!session) throw redirect(302, '/login');
+		if (!session) throw redirectToSignIn({ url, error: 'auth_required' });
 		if (session.user.email_verified) {
 			throw redirect(302, '/');
 		}
