9679f7611b6ed8cb53d32c5759e46db2baeebdd5
Sikriti Dakua <destrodev123@gmail.com>
2023-09-08 15:17:19 +0530

    update: `alert` props & styles

--- src/components/ui/alert/alert.svelte
+++ src/components/ui/alert/alert.svelte
@@ -2,17 +2,44 @@
 	import { cn } from '$lib/utils';
 	import type { HTMLAttributes } from 'svelte/elements';
 	import { alertVariants, type Variant } from '.';
+	import { Button } from '$components/ui/button';
+	import { XIcon } from 'lucide-svelte';
 
 	type $$Props = HTMLAttributes<HTMLDivElement> & {
 		variant?: Variant;
+		dismissable?: boolean;
 	};
 
 	let className: $$Props['class'] = undefined;
-	export let variant: $$Props['variant'] = 'default';
-
 	export { className as class };
+	export let variant: $$Props['variant'] = 'default';
+	export let dismissable: $$Props['dismissable'] = false;
+
+	let open = true;
+
+	function close(e: MouseEvent) {
+		e.stopPropagation();
+		open = false;
+	}
 </script>
 
-<div class={cn(alertVariants({ variant }), className)} {...$$restProps} role="alert">
-	<slot />
-</div>
+{#if open}
+	<div
+		class={cn(alertVariants({ variant, className }), dismissable && 'pr-8')}
+		{...$$restProps}
+		role="alert"
+	>
+		<slot />
+		{#if dismissable}
+			<Button
+				type="button"
+				variant="ghost"
+				size="icon"
+				class="square-6 absolute right-2 top-1/2 -translate-y-1/2 hover:bg-foreground/10 hover:text-inherit"
+				on:click={close}
+			>
+				<XIcon class="square-4" />
+			</Button>
+		{/if}
+	</div>
+{/if}
--- src/components/ui/alert/index.ts
+++ src/components/ui/alert/index.ts
@@ -4,23 +4,18 @@ import Root from './alert.svelte';
 import Description from './alert-description.svelte';
 import Title from './alert-title.svelte';
 
-export const alertVariants = cva(
-	'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
-	{
-		variants: {
-			variant: {
-				default: 'bg-background text-foreground',
-				destructive:
-					'bg-destructive-muted border-destructive-muted-foreground text-destructive-muted-foreground [&>svg]:text-destructive-muted-foreground',
-				success:
-					'bg-success-muted border-success-muted-foreground text-success-muted-foreground [&>svg]:text-success-muted-foreground'
-			}
-		},
-		defaultVariants: {
-			variant: 'default'
+export const alertVariants = cva('relative w-full rounded-lg border p-4 text-sm', {
+	variants: {
+		variant: {
+			default: 'bg-background text-foreground [&>svg]:text-foreground',
+			destructive: 'bg-destructive/10 border-destructive text-destructive [&>svg]:text-destructive',
+			success: 'bg-success/10 border-success text-success [&>svg]:text-success'
 		}
+	},
+	defaultVariants: {
+		variant: 'default'
 	}
-);
+});
 
 export type Variant = VariantProps<typeof alertVariants>['variant'];
 export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
