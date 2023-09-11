091137418f88f6099dfaec15e68eeaac5fa5b260
Sikriti Dakua <destrodev123@gmail.com>
2023-09-12 00:53:27 +0530

    update: input `readonly` for logged users

--- src/routes/(public)/forgot-password/+page.svelte
+++ src/routes/(public)/forgot-password/+page.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+
+	import { onMount } from 'svelte';
+	import { superForm } from 'sveltekit-superforms/client';
+
+	import { currentUser } from '$lib/store';
+
 	import { Button } from '$components/ui/button';
 	import { Card, CardContent, CardFooter, CardHeader } from '$components/ui/card';
 	import { TextInput } from '$components/ui/text-input';
-	import { toast } from '$components/ui/toast/toaster.svelte';
-	import { superForm } from 'sveltekit-superforms/client';
+	import { toast } from '$components/ui/toast';
+	import { Skeleton } from '$components/ui/skeleton';
 
-	export let data;
+	export let data: PageData;
 
-	const { form, errors, enhance, submitting, constraints } = superForm(data.form, {
+	let visible = false;
+
+	const { form, errors, enhance, submitting, constraints, validate } = superForm(data.form, {
 		onUpdated({ form }) {
 			if (form.message?.type === 'success') toast.success(form.message.message);
 			if (form.message?.type === 'error') toast.error(form.message.message);
-		},
-		onError(event) {
-			console.log(event);
 		}
+	});
+
+	onMount(() => {
+		visible = true;
+		if ($currentUser) validate('email', { value: $currentUser.email });
 	});
 </script>
 
@@ -33,22 +44,25 @@
 			</p>
 		</CardHeader>
 		<CardContent>
-			<form method="post" use:enhance>
-				<div class="space-y-4">
-					<TextInput
-						label="Email"
-						type="email"
-						name="email"
-						placeholder="Enter your email"
-						bind:value={$form.email}
-						error={$errors.email?.[0]}
-						disabled={$submitting}
-						id="email"
-						{...$constraints.email}
-					/>
-					<Button class="w-full" loading={$submitting}>Reset Password</Button>
-				</div>
-			</form>
+			<Skeleton {visible}>
+				<form method="post" use:enhance>
+					<div class="space-y-4">
+						<TextInput
+							label="Email"
+							type="email"
+							name="email"
+							placeholder="Enter your email"
+							bind:value={$form.email}
+							error={$errors.email?.[0]}
+							disabled={$submitting}
+							id="email"
+							{...!!$currentUser && { readonly: true }}
+							{...$constraints.email}
+						/>
+						<Button class="w-full" loading={$submitting}>Reset Password</Button>
+					</div>
+				</form>
+			</Skeleton>
 		</CardContent>
 		<CardFooter>
 			<div class="flex items-center gap-2">
