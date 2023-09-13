8421b287cd1319dccc0eefe92ad88f011bab8db7
Sikriti Dakua <destrodev123@gmail.com>
2023-09-14 00:57:49 +0530

    feat: add `email` components

---
+++ src/components/emails/reset-password.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button, Hr, Html, Text } from 'svelte-email';
+
+	export let email: string;
+	export let url: string;
+</script>
+
+<Html lang="en">
+	<Text>
+		Hello {email},<br />
+		<br />
+		You have requested to reset you password<br />
+		<br />
+		Please click the button to reset your password
+	</Text>
+	<Hr />
+	<Button href={url}>Reset password</Button>
+</Html>
---
+++ src/components/emails/welcome.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Html, Text } from 'svelte-email';
+
+	export let email: string;
+</script>
+
+<Html lang="en">
+	<Text>
+		Hello {email},<br />
+		<br />
+		Welcome to todoit. make your todos
+	</Text>
+</Html>
