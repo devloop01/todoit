7036f7b668a17b2b081db32f16b8ee2f09da773a
Sikriti Dakua <destrodev123@gmail.com>
2023-09-14 00:17:23 +0530

    update: `input` component styles

--- src/components/ui/input/input.svelte
+++ src/components/ui/input/input.svelte
@@ -1,7 +1,7 @@
 <script context="module" lang="ts">
 	export const inputVariants = cva(
 		[
-			'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+			'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors transition-shadow file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
 			'data-[error="true"]:border-destructive data-[error="true"]:placeholder:text-destructive data-[error="true"]:focus-visible:ring-destructive'
 		],
 		{
