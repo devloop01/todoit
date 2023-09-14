5e24e6b1e74642eb932e1bd5f1a1835e7a2e32fe
Sikriti Dakua <destrodev123@gmail.com>
2023-09-14 23:38:51 +0530

    update: add fade `transition`

--- src/components/ui/alert/alert.svelte
+++ src/components/ui/alert/alert.svelte
@@ -4,6 +4,7 @@
 	import { alertVariants, type Variant } from '.';
 	import { Button } from '$components/ui/button';
 	import { XIcon } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
 
 	type $$Props = HTMLAttributes<HTMLDivElement> & {
 		variant?: Variant;
@@ -23,23 +24,26 @@
 	}
 </script>
 
-{#if open}
-	<div
-		class={cn(alertVariants({ variant, className }), dismissable && 'pr-8')}
-		{...$$restProps}
-		role="alert"
-	>
-		<slot />
-		{#if dismissable}
-			<Button
-				type="button"
-				variant="ghost"
-				size="icon"
-				class="sq-6 absolute right-2 top-1/2 -translate-y-1/2 hover:bg-foreground/10 hover:text-inherit"
-				on:click={close}
-			>
-				<XIcon class="sq-4" />
-			</Button>
-		{/if}
-	</div>
-{/if}
+<div in:fade={{ duration: 100 }}>
+	{#if open}
+		<div
+			class={cn(alertVariants({ variant, className }), dismissable && 'pr-8')}
+			{...$$restProps}
+			role="alert"
+			out:fade={{ duration: 100 }}
+		>
+			<slot />
+			{#if dismissable}
+				<Button
+					type="button"
+					variant="ghost"
+					size="icon"
+					class="absolute right-2 top-1/2 -translate-y-1/2 sq-6 hover:bg-foreground/10 hover:text-inherit"
+					on:click={close}
+				>
+					<XIcon class="sq-4" />
+				</Button>
+			{/if}
+		</div>
+	{/if}
+</div>
