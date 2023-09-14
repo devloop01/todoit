4237506980e86c8633add67f35f71f6b6b802744
Sikriti Dakua <destrodev123@gmail.com>
2023-09-15 01:20:56 +0530

    update: `getBaseUrl` method

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -1,16 +1,8 @@
 import { env } from '$env/dynamic/private';
 
 export function getBaseUrl() {
-	if (typeof window !== 'undefined')
-		// browser should use relative path
-		return '';
-	if (env.VERCEL_URL)
-		// reference for vercel.com
-		return `https://${env.VERCEL_URL}`;
-	if (env.RENDER_INTERNAL_HOSTNAME)
-		// reference for render.com
-		return `http://${env.RENDER_INTERNAL_HOSTNAME}:${env.PORT}`;
-	// assume localhost
+	if (typeof window !== 'undefined') return '';
+	if (env.SITE_URL) return `https://${env.SITE_URL}`;
 	return `http://localhost:${env.PORT ?? 5173}`;
 }
 
