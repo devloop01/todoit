7e995f337fb548733d8432c7ca42b269160abb96
Sikriti Dakua <destrodev123@gmail.com>
2023-09-14 23:58:13 +0530

    update: `auth` schema

--- src/lib/schema/auth.ts
+++ src/lib/schema/auth.ts
@@ -10,36 +10,36 @@ export const signInSchema = z.object({
 	password: z.string().min(1, 'Password is required')
 });
 
-export const signUpSchema = z
-	.object({
-		name: z
-			.string()
-			.min(1, 'Name is required')
-			.max(32, 'Name cannot have more than 32 characters')
-			.regex(Regex.alphabetsAndSpace, 'Name cannot have numbers or special characters')
-			.trim(),
-		email: z
-			.string()
-			.min(1, 'Email is required')
-			.max(64, 'Email cannot have more than 64 characters')
-			.email('Email is invalid'),
-		password: z
-			.string()
-			.min(1, 'Password is required')
-			.max(64, 'Password cannot have more than 64 characters')
-			.regex(
-				Regex.password,
-				'Minimum 7 characters, atleast 1 Alphabet, 1 Number and 1 Special Character'
-			),
-		confirmPassword: z.string().min(1, 'Confirm password is required')
-	})
-	.superRefine((data, ctx) => {
-		if (data.confirmPassword !== data.password) {
-			ctx.addIssue({
-				code: 'custom',
-				message: "passwords don't match",
-				path: ['confirmPassword']
-			});
-		}
-	});
+export const signUpSchemaRaw = z.object({
+	name: z
+		.string()
+		.min(1, 'Name is required')
+		.max(32, 'Name cannot have more than 32 characters')
+		.regex(Regex.alphabetsAndSpace, 'Name cannot have numbers or special characters')
+		.trim(),
+	email: z
+		.string()
+		.min(1, 'Email is required')
+		.max(64, 'Email cannot have more than 64 characters')
+		.email('Email is invalid'),
+	password: z
+		.string()
+		.min(1, 'Password is required')
+		.max(64, 'Password cannot have more than 64 characters')
+		.regex(
+			Regex.password,
+			'Minimum 7 characters, atleast 1 Alphabet, 1 Number and 1 Special Character'
+		),
+	confirmPassword: z.string().min(1, 'Confirm password is required')
+});
+
+export const signUpSchema = signUpSchemaRaw.superRefine((data, ctx) => {
+	if (data.confirmPassword !== data.password) {
+		ctx.addIssue({
+			code: 'custom',
+			message: "passwords don't match",
+			path: ['confirmPassword']
+		});
+	}
+});
 
