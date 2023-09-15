a70744966ff7666c25367f83258e6addcc16c1cb
Sikriti Dakua <destrodev123@gmail.com>
2023-09-15 16:04:09 +0530

    update: switch to `vercel` adapter

--- src/lib/server/utils.ts
+++ src/lib/server/utils.ts
@@ -3,6 +3,7 @@ import { env } from '$env/dynamic/private';
 export function getBaseUrl() {
 	if (typeof window !== 'undefined') return '';
 	if (env.SITE_URL) return `https://${env.SITE_URL}`;
+	if (env.VERCEL_URL) return `https://${env.VERCEL_URL}`;
 	return `http://localhost:${env.PORT ?? 5173}`;
 }
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
